69576129ac5c0848a2d9b1e713ba9389d7beeaf7
karuta kun <sreenu.1009daf@gmail.com>
2025-02-22 18:59:02 +0530

    Signed-off-by: karuta kun <sreenu.1009daf@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite"
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/personal-portfolio",
+  server:{
+    host: true,
+    strictPort: true,
+    port: 5173
+  }
 })
+
